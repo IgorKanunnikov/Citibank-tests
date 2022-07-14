c05b4abab4619aedd9278ea5db51534d80df78a1
Igor Kanunnikov <ivkqqa@gmail.com>
2022-07-14 22:04:52 +0300

    refactoring TestBase

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -20,21 +20,9 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 @ExtendWith({AllureJunit5.class})
 public class TestBase {
-    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
     @BeforeAll
     static void beforeAll() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
-        Configuration.browser = System.getProperty("browser","chrome");
-        Configuration.browserVersion = System.getProperty("version","100");
-        Configuration.browserSize = System.getProperty("browserSize", "3840x2160");
-        // Configuration.holdBrowserOpen = true;
-        Configuration.remote = String.format("https://%s:%s@%s", config.login(), config.password(), config.urlCloudSelenoid());
+        DriverSettings.configure();
     }
     @BeforeEach
     public void beforeEach() {
